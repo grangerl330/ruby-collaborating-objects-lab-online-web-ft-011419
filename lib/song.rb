203594ef64b04a8b60b9499f811a0ff09f83107e
Luke Granger <grangerl330@gmail.com>
2019-02-02 21:14:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,17 @@
-class Song 
-  attr_accessor :name, :artist 
-  
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist
+
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
-  
+    @name = name
+    @@all << self
+  end
+
   def self.new_by_filename(filename)
-    song = self.new(filename)
-    song.name = filename.split(" - ")[1]
-    song.artist = Artist.new(filename.split(" - ")[0])
-    song.artist.add_song(song)
-    song 
-  end 
- 
-end 
+    binding.pry 
+  end
+
+end
